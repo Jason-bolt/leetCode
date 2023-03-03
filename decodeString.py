af0e069c5fc669fbae63876deb6d97846482d2ad
Jason Appiatu <appiatujason@gmail.com>
2023-03-03 07:30:15 +0000

    Fixed bugs

--- decodeString.py
+++ decodeString.py
@@ -1,5 +1,9 @@
-from ast import List
+"""
+Author: Jason Kwame Appiatu
+Latest Update Date: 3rd, March, 2023
+"""
 
+from ast import List
 
 def decodeString(s: str) -> str:
     if s.find('[') != -1:
@@ -59,16 +63,17 @@ def decodeString(s: str) -> str:
         elif (first_bracket > 1 and second_bracket == len(s)-1):
             # print(s[0:first_bracket - 1] + (num * content) + s[second_bracket + 1:])
             return decodeString(s[0:first_bracket - len(str(num))] + (num * content))
+    elif s.isdigit():
+        return ""
     else:
-        if (s[0].isdigit()):
-            s = s[1:]
-        if (s[0].isdigit()):
-            s = s[1:]
         return s
 
 
+
+# TEST CASES
+
 # test_string = "300[a]"
-test_string = "3[2[a]]"
+test_string = "300[2[a]]"
 # test_string = "3[a]2[bc]"
 # test_string = "3[a2[c]]4[cds]dvf"
 # test_string = "2[abc]3[cd]ef"
