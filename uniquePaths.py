0dc904004f18154c1c9eca4bd1c676760b2d6d4b
Jason Appiatu <appiatujason@gmail.com>
2023-05-15 09:13:01 +0000

    Changed codes

--- uniquePaths.py
+++ uniquePaths.py
@@ -1,10 +1,11 @@
 def uniquePaths(m: int, n: int) -> int:
-    def recur(m=m, n=n, i=0, j=0):
-        if i >= m or j >= n:
-            return 0
-        if i == m-1 and j == n-1:
-            return 1
-        return recur(m, n, i+1, j) + recur(m, n, i, j+1)
-    return recur()
-    
+    row = [1] * n
+    for i in range(m - 1):
+        newRow = [1] * n
+        for j in range(n - 2, -1, -1):
+            newRow[j] = newRow[j + 1] + row[j]
+        row = newRow
+    return row[0]
+
+
 print(uniquePaths(3, 7))
