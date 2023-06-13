ef193930a1b09336a3202b2790311fe3575329b2
Jason Appiatu <appiatujason@gmail.com>
2023-06-13 12:50:43 +0000

    Updated remove nth number

--- removeNthFromEnd.py
+++ removeNthFromEnd.py
@@ -30,3 +30,30 @@ class Solution:
                 newHead.next = ListNode()
                 newHead = newHead.next
         return head2
+    
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        first, second, prev = head, head, head
+        count = 0
+        while first.next != None:
+            if count != n - 1 and first.next != None:
+                first = first.next
+                count += 1
+                continue
+            if first.next != None:
+                prev = second
+                first = first.next
+                second = second.next
+                if (first.next == None):
+                    prev.next = prev.next.next
+                    return head
+        if count + 1 == n:
+            return head.next
+            
