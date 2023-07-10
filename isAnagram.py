7ca74fe958d1bfc17d59562b0319682ec9078a34
Jason Appiatu <appiatujason@gmail.com>
2023-07-10 08:07:16 +0000

    isAnagram

---
+++ isAnagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        print(s, t)
+        return s == t
